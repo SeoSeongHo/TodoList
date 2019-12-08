b9258caeb26da25ea2b1bbcff6e1f829e3995470
Seo Seong Ho <swa04246@naver.com>
2019-12-08 17:25:40 +0900

    add controller
    * add Todo Controller

--- todo/src/main/kotlin/com/todo/controller/TodoController.kt
+++ todo/src/main/kotlin/com/todo/controller/TodoController.kt
@@ -1,5 +1,7 @@
 package com.todo.controller
-
+import com.todo.dto.TodoDto
+import com.todo.services.todoRegister.ITodoRegisterService
+import com.todo.services.todoRegister.TodoRegisterService
 import org.springframework.beans.factory.annotation.*
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
@@ -7,10 +9,25 @@ import org.springframework.web.bind.annotation.*
 @Controller
 @RequestMapping("api/todo")
 class TodoController (
-        @Autowired private val _todoContext: todoContext
+        @Autowired private val todoRegisterService: ITodoRegisterService
 ){
     @GetMapping("/")
-    fun todolist() : {
+    fun getTodoList() : List<TodoDto>{
+        return todoRegisterService.getTodoList()
+    }
 
+    @GetMapping("/{id}")
+    fun getTodo(@PathVariable id : Long) : TodoDto{
+        return todoRegisterService.getTodo(id)
+    }
+
+    @PostMapping("/")
+    fun updateTodo(todoDto: TodoDto) : TodoDto{
+        return todoRegisterService.postTodo(todoDto)
+    }
+
+    @DeleteMapping("/{id}")
+    fun removeTodo(@PathVariable id : Long) {
+        return todoRegisterService.deleteTodo(id)
     }
 }
